785d059c7d2ece1d39c0233ab4e9be86f2241c4d
trunksBT <korytko07@gmail.com>
2019-11-26 22:20:31 +0100

    Extract submodule database (#6)
    * Remove the source code of dtabase
    
    * Add the Database as submodule
    
    * Add include_directories for Database

--- src/Database/Objects/IOPaths.cpp
+++
@@ -1,9 +0,0 @@
-#include <Database/Objects/IOPaths.hpp>
-
-std::ostream& operator << (std::ostream& os, const IOPaths& rhs)
-{
-    os << "bufferToSend " << rhs.bufferToSend;
-    return os;
- 
-}
-
