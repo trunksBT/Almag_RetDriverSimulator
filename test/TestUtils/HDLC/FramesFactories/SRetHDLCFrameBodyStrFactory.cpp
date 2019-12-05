03de26c15d0b296ddd4f66b82ab36d53a8914033
pkorycin <pkorycin>
2019-12-05 17:23:47 +0100

    Finish using the HDLCFrameBodyStr factory in HDLCFrameStr factory

--- test/TestUtils/HDLC/FramesFactories/SRetHDLCFrameBodyStrFactory.cpp
+++ test/TestUtils/HDLC/FramesFactories/SRetHDLCFrameBodyStrFactory.cpp
@@ -1,44 +1,32 @@
 #include "SRetHDLCFrameBodyStrFactory.hpp"
 
-namespace
-{
-const std::string CALIBRATE_STR{ "3 fe 31 " };
-const std::string LINK_ESTABLISHMENT{ "3 93 " };
-const std::string DUMMY_SCAN_FRAME{ "ff bf 81 f0 8 1 2 33 33 3 2 ff ff " };
-const std::string ADDRESS_ASSIGNMENT_FRAME{
-   "ff bf 81 f0 11 1 9 4e 4b 34 "
-   "36 35 30 30 30 30 2 1 3 4 1 1"
-};
-const std::string THREEGPP_RELEASE_ID = "3 bf 81 f0 3 5 1 a ";
-const std::string AISG_PROTOCOL_VERSION{ "3 bf 81 f0 3 14 1 2 " };
-}
-
 std::string SRetHDLCFrameBodyStrFactory::get_FrameI_Calibrate()
 {
-   return CALIBRATE_STR;
+   return { "3 fe 31 " };
 }
 
 std::string SRetHDLCFrameBodyStrFactory::get_FrameSNRM_LinkEstablishment()
 {
-   return LINK_ESTABLISHMENT;
+   return { "3 93 " };
 }
 
 std::string SRetHDLCFrameBodyStrFactory::get_FrameXID_DummyScan()
 {
-   return DUMMY_SCAN_FRAME;
+   return { "ff bf 81 f0 8 1 2 33 33 3 2 ff ff " };
 }
 
 std::string SRetHDLCFrameBodyStrFactory::get_FrameXID_AddressAssignment()
 {
-   return ADDRESS_ASSIGNMENT_FRAME;
+   return {"ff bf 81 f0 11 1 9 4e 4b 34 "
+           "36 35 30 30 30 30 2 1 3 4 1 1 " };
 }
 
 std::string SRetHDLCFrameBodyStrFactory::get_FrameXID_3GPPReleaseId()
 {
-   return THREEGPP_RELEASE_ID;
+   return { "3 bf 81 f0 3 5 1 a " };
 }
 
 std::string SRetHDLCFrameBodyStrFactory::get_FrameXID_AISGProtocolVersion()
 {
-   return AISG_PROTOCOL_VERSION;
+   return { "3 bf 81 f0 3 14 1 2 " };
 }
--- test/TestUtils/HDLC/FramesFactories/SRetHDLCFrameStrFactory.cpp
+++ test/TestUtils/HDLC/FramesFactories/SRetHDLCFrameStrFactory.cpp
@@ -7,18 +7,6 @@
 using namespace std::string_literals;
 using namespace printUtils;
 
-namespace
-{
-const std::string CALIBRATE_STR = "7e 3 fe 31 13 37 7e ";
-const std::string LINK_ESTABLISHMENT = "7e 3 93 13 37 7e ";
-const std::string DUMMY_SCAN_FRAME = "7e ff bf 81 f0 8 1 2 33 33 3 2 ff ff 13 37 7e ";
-const std::string ADDRESS_ASSIGNMENT_FRAME =
-        "7e ff bf 81 f0 11 1 9 4e 4b 34 "
-        "36 35 30 30 30 30 2 1 3 4 1 1 13 37 7e ";
-const std::string THREEGPP_RELEASE_ID_HDLC_FRAME_STR = "7e 3 bf 81 f0 3 5 1 a 13 37 7e ";
-const std::string AISG_PROTOCOL_VERSION_HDLC_FRAME_STR = "7e 3 bf 81 f0 3 14 1 2 13 37 7e ";
-}
-
 SRetHDLCFrameStrFactory::SRetHDLCFrameStrFactory()
 : sRetHdlcFrameBodyStrFactory_(std::make_shared<SRetHDLCFrameBodyStrFactory>())
 {}
@@ -26,31 +14,41 @@ SRetHDLCFrameStrFactory::SRetHDLCFrameStrFactory()
 std::string SRetHDLCFrameStrFactory::get_FrameI_Calibrate()
 {
    return toString(START_STOP_FLAG)
-      + sRetHdlcFrameBodyStrFactory_->get_FrameI_Calibrate()
-      + toString(CRC) + toString(START_STOP_FLAG);
+         + sRetHdlcFrameBodyStrFactory_->get_FrameI_Calibrate()
+         + toString(CRC) + toString(START_STOP_FLAG);
 }
 
 std::string SRetHDLCFrameStrFactory::get_FrameSNRM_LinkEstablishment()
 {
-   return "7e "s + sRetHdlcFrameBodyStrFactory_->get_FrameSNRM_LinkEstablishment() + "13 37 7e "s;
+   return toString(START_STOP_FLAG)
+          + sRetHdlcFrameBodyStrFactory_->get_FrameSNRM_LinkEstablishment()
+          + toString(CRC) + toString(START_STOP_FLAG);
 }
 
 std::string SRetHDLCFrameStrFactory::get_FrameXID_DummyScan()
 {
-   return DUMMY_SCAN_FRAME;
+   return toString(START_STOP_FLAG)
+          + sRetHdlcFrameBodyStrFactory_->get_FrameXID_DummyScan()
+          + toString(CRC) + toString(START_STOP_FLAG);
 }
 
 std::string SRetHDLCFrameStrFactory::get_FrameXID_AddressAssignment()
 {
-   return ADDRESS_ASSIGNMENT_FRAME;
+   return toString(START_STOP_FLAG)
+          + sRetHdlcFrameBodyStrFactory_->get_FrameXID_AddressAssignment()
+          + toString(CRC) + toString(START_STOP_FLAG);
 }
 
 std::string SRetHDLCFrameStrFactory::get_FrameXID_3GPPReleaseId()
 {
-   return THREEGPP_RELEASE_ID_HDLC_FRAME_STR;
+   return toString(START_STOP_FLAG)
+          + sRetHdlcFrameBodyStrFactory_->get_FrameXID_3GPPReleaseId()
+          + toString(CRC) + toString(START_STOP_FLAG);
 }
 
 std::string SRetHDLCFrameStrFactory::get_FrameXID_AISGProtocolVersion()
 {
-   return AISG_PROTOCOL_VERSION_HDLC_FRAME_STR;
+   return toString(START_STOP_FLAG)
+          + sRetHdlcFrameBodyStrFactory_->get_FrameXID_AISGProtocolVersion()
+          + toString(CRC) + toString(START_STOP_FLAG);
 }
