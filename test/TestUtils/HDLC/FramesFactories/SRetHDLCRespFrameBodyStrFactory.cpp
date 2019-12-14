8a158ba6e2243f2e0f5fe2ebab0befba108c6c45
pkorycin <pkorycin>
2019-12-14 19:58:31 +0100

    Throw the exception when no implemented frame

--- test/TestUtils/HDLC/FramesFactories/SRetHDLCRespFrameBodyStrFactory.cpp
+++ test/TestUtils/HDLC/FramesFactories/SRetHDLCRespFrameBodyStrFactory.cpp
@@ -2,31 +2,30 @@
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameI_Calibrate()
 {
-   return { "3 fe 31 " };
+   throw std::runtime_error("Not implemented yet");
 }
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameU_LinkEstablishment()
 {
-   return { "3 93 " };
+   return { "3 73 " };
 }
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameXID_DummyScan()
 {
-   return { "ff bf 81 f0 8 1 2 33 33 3 2 ff ff " };
+   throw std::runtime_error("Not implemented yet");
 }
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameXID_AddressAssignment()
 {
-   return {"ff bf 81 f0 11 1 9 4e 4b 34 "
-           "36 35 30 30 30 30 2 1 3 4 1 1 " };
+   throw std::runtime_error("Not implemented yet");
 }
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameXID_3GPPReleaseId()
 {
-   return { "3 bf 81 f0 3 5 1 a " };
+   throw std::runtime_error("Not implemented yet");
 }
 
 std::string SRetHDLCRespFrameBodyStrFactory::get_FrameXID_AISGProtocolVersion()
 {
-   return { "3 bf 81 f0 3 14 1 2 " };
+   throw std::runtime_error("Not implemented yet");
 }
